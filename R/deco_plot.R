88874b3cbef331c42f064a1db2797151a0ecbc96
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2024-01-16 14:26:36 +0100

    build

--- R/deco_plot.R
+++ R/deco_plot.R
@@ -1,50 +1,50 @@
-#' @title deco_plot
-#' @description Creates a decomposition plot
+#' deco_plot
+#'
+#' Plot for decomposition analysis.
+#' Use function deco in library magpie4 to create the necessary object.
+#'
 #' @param x magpie object with decomposition data (e.g. from the function magpie4:::deco)
 #' @param color color vector
 #' @return a ggplot object
-#' @author Benjamin Leon Bodirsky, Ina Neher
-#' @export
-#' 
+#'
 #' @importFrom grDevices gray.colors
 #' @importFrom ggplot2 ggplot aes geom_bar geom_point ggplot labs scale_fill_manual
 #' @importFrom quitte as.quitte
-#' @importFrom magclass `getSets<-`
-#' 
-#' 
+#' @importFrom magclass getSets<-
+#' @author Benjamin Leon Bodirsky, Ina Neher
+#' @export
+deco_plot <- function(x, color = gray.colors(6)) { # nolint: object_name_linter.
+  getSets(x) <- c("region", "year", "Decomposition")
+  legendNames <- getNames(x)
+  x <- x[, , rev(legendNames)]
+  color <- rev(color)[seq_along(legendNames)]
 
-# Plot for decomposition analysis
-# use function deco in library magpie4 to create the necessary object.
-deco_plot<-function(x, color=gray.colors(6)){
-  getSets(x)<-c("region","year","Decomposition")
-  legend_names<-getNames(x)
-  x<-x[,,rev(legend_names)]
-  color<-rev(color)[1:length(legend_names)]
-  
-  x1<- as.quitte(x[,,1:(length(getNames(x))-1)])
-  x2<- as.quitte(x[,,length(getNames(x))])
-  class(x1) <- setdiff(class(x1),"data.table")
-  class(x2) <- setdiff(class(x2),"data.table")
-  x1$region <- factor(x1$region, levels=getRegions(x))
-  x2$region <- factor(x2$region, levels=getRegions(x))
-  
-  years=getYears(x,as.integer = T)
-  yeardiff=unique(years[2:length(years)]-years[1:(length(years)-1)])
-  if(length(yeardiff)>1) {stop("timesteps have to have equal length")}
-  
-  x1$period=x1$period-yeardiff/2
-  x2$period=x2$period-yeardiff/2
-  
-  p <- ggplot() 
-  p <- p + geom_bar(data=x1,size=1,stat="identity",position="stack",aes_string(x="period",y="value",fill="Decomposition")) 
-  p <- p + geom_line(data=x2,aes_string(x="period",y="value"),color="white",size=1.2)
-  p <- p + geom_line(data=x2,aes_string(x="period",y="value"),color=color[length(legend_names)],size=0.8)
-  p <- p+scale_x_continuous(minor_breaks=c(years[1]-yeardiff,years))
+  x1 <- as.quitte(x[, , 1:(length(getNames(x)) - 1)])
+  x2 <- as.quitte(x[, , length(getNames(x))])
+  class(x1) <- setdiff(class(x1), "data.table")
+  class(x2) <- setdiff(class(x2), "data.table")
+  x1$region <- factor(x1$region, levels = getItems(x, 1.1))
+  x2$region <- factor(x2$region, levels = getItems(x, 1.1))
+
+  years <- getYears(x, as.integer = TRUE)
+  yeardiff <- unique(years[2:length(years)] - years[1:(length(years) - 1)])
+  if (length(yeardiff) > 1) {
+    stop("timesteps have to have equal length")
+  }
+
+  x1$period <- x1$period - yeardiff / 2
+  x2$period <- x2$period - yeardiff / 2
+
+  p <- ggplot()
+  p <- p + geom_bar(data = x1, size = 1, stat = "identity", position = "stack",
+                    aes_string(x = "period", y = "value", fill = "Decomposition"))
+  p <- p + geom_line(data = x2, aes_string(x = "period", y = "value"), color = "white", size = 1.2)
+  p <- p + geom_line(data = x2, aes_string(x = "period", y = "value"), color = color[length(legendNames)], size = 0.8)
+  p <- p + scale_x_continuous(minor_breaks = c(years[1] - yeardiff, years))
   p <- p + theme(panel.grid.minor.x =  element_line(colour = "grey", size = 0.25),
                  panel.grid.major.x =  element_line(colour = "#666666ff", size = 0.5))
-  p <- p + facet_wrap(~ region, ncol=2)
-  p <- p + scale_fill_manual(values = color) 
-  #  p <- p + scale_y_continuous(limits = c(0,y_limit), labels=NULL,expand = c(0, 0)) + theme(axis.ticks.y = element_blank(),axis.text.x  = element_text(angle=90,size=8))
-  p <- p + labs(y = "percent/timestep", x = "year") 
+  p <- p + facet_wrap(~region, ncol = 2)
+  p <- p + scale_fill_manual(values = color)
+  p <- p + labs(y = "percent/timestep", x = "year")
   return(p)
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Landuse Plot Library
 
-R package **luplot**, version **3.56.0**
+R package **luplot**, version **3.56.1**
 
 [![CRAN status](https://www.r-pkg.org/badges/version/luplot)](https://cran.r-project.org/package=luplot)  [![R build status](https://github.com/pik-piam/luplot/workflows/check/badge.svg)](https://github.com/pik-piam/luplot/actions) [![codecov](https://codecov.io/gh/pik-piam/luplot/branch/master/graph/badge.svg)](https://app.codecov.io/gh/pik-piam/luplot) [![r-universe](https://pik-piam.r-universe.dev/badges/luplot)](https://pik-piam.r-universe.dev/builds)
 
@@ -39,7 +39,7 @@ In case of questions / problems please contact Benjamin Bodirsky <bodirsky@pik-p
 
 To cite package **luplot** in publications use:
 
-Bodirsky B, Dietrich J, Krause M, Stevanovic M, Humpenoeder F, Weindl I, Baumstark L, Klein D, Rolinski S, Wang X (2023). _luplot: Landuse Plot Library_. R package version 3.56.0, <https://github.com/pik-piam/luplot>.
+Bodirsky B, Dietrich J, Krause M, Stevanovic M, Humpenoeder F, Weindl I, Baumstark L, Klein D, Rolinski S, Wang X (2024). _luplot: Landuse Plot Library_. R package version 3.56.1, <https://github.com/pik-piam/luplot>.
 
 A BibTeX entry for LaTeX users is
 
@@ -47,8 +47,8 @@ A BibTeX entry for LaTeX users is
 @Manual{,
   title = {luplot: Landuse Plot Library},
   author = {Benjamin Leon Bodirsky and Jan Philipp Dietrich and Michael Krause and Miodrag Stevanovic and Florian Humpenoeder and Isabelle Weindl and Lavinia Baumstark and David Klein and Susanne Rolinski and Xiaoxi Wang},
-  year = {2023},
-  note = {R package version 3.56.0},
+  year = {2024},
+  note = {R package version 3.56.1},
   url = {https://github.com/pik-piam/luplot},
 }
 ```
